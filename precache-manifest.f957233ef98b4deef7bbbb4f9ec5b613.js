ead94ea35e3df48d97ee695d22dfc022b9065846
monkey <daonamtien1@gmail.com>
2022-06-14 13:22:06 +0700

    Updates

---
+++ precache-manifest.f957233ef98b4deef7bbbb4f9ec5b613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3a95acccf5c69e097787f2053b33ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "def6c67ea9bf38c141f5",
+    "url": "/static/css/2.2dc0ed06.chunk.css"
+  },
+  {
+    "revision": "ccd65c5a1549328a632a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "def6c67ea9bf38c141f5",
+    "url": "/static/js/2.36a14ce6.chunk.js"
+  },
+  {
+    "revision": "ccd65c5a1549328a632a",
+    "url": "/static/js/main.41f1f589.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
